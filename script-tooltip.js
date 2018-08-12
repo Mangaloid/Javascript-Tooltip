2f0122637769620d4e8d5b77caeef170d2f02787
Mangaloid <42314333+Mangaloid@users.noreply.github.com>
2018-08-12 17:31:59 +0800

    Add files via upload

---
+++ script-tooltip.js
@@ -0,0 +1,31 @@
+<script type="text/javascript">
+$(document).ready(function()
+{
+  $('.tooltip-pos').each(function(){
+
+    var $this = $(this);
+    var id = $this.attr('rel');
+
+    $this.qtip({
+      content:{
+        text: '<img class="" src="https://mangaloid.id/wp-content/themes/Mangaloid/images/loader.gif" alt="Loading..." />',
+        ajax:{
+          url: 'https://mangaloid.id/wp-content/themes/Mangaloid/tooltip.php',
+          type: 'POST', 
+          loading: false,
+          data: 'id=' + id
+        }
+      },
+      show: 'mouseover',
+      hide: {
+        delay: 200,
+        fixed: true
+      },
+      position: {
+	target: 'mouse',
+        viewport: $(window)
+      }
+    });
+  });
+});
+</script>
